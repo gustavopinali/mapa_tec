ce9161108ca4705e5e3fc64f1e44603789eaf14e
Gustavo Pinali <gustavopinali@gmail.com>
2024-06-30 20:28:11 -0300

    Segundo commit: Modificando projeto HelloWorld

--- HelloWord.java
+++ HelloWord.java
@@ -1,6 +1,6 @@
 public class HelloWord {
 
     public static void main(String[] args){
-        System.out.println("Olá Mundo! - Mapa - 20023484-5");
+        System.out.println("Olá Mundo! 2 - Mapa - 20023484-5");
     }
 }
